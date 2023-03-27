6f3a6668cb4c81de11a8f1a086660d307f26b08e
DAVID <davidcasallas013@gmail.com>
2023-03-27 10:39:14 -0500

    Enabled middlewares

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ from fastapi import FastAPI
 from models.request_body import BaseBody, OperationBody
 from utils.number_set import get_number_base
 from utils.operations import addition, subtraction, multiplication, division
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/base/")
 async def get_base(num: BaseBody):
